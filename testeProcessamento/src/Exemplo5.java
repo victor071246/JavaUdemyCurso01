e70ee6bd6a46510d137eb5286aefeb5d1f399714
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2024-10-05 21:05:34 -0300

    Add files via upload

---
+++ testeProcessamento/src/Exemplo5.java
@@ -0,0 +1,12 @@
+public class Exemplo5 {
+    public static void main(String[] args) {
+
+        double a;
+        int b;
+
+        a = 5.0;
+        b = (int) a;
+
+        System.out.println(b);
+    }
+}
